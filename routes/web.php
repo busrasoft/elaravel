cc58041bd45af2e4292fe3d7e28b078115408687
unknown <busrasof@gmail.com>
2020-02-17 22:06:52 -0800

    collection metodlari kullanimlari

--- routes/web.php
+++ routes/web.php
@@ -111,7 +111,161 @@ Route::any('workInsert','WorkController@workInsert')->name('workInsert');
 
 
 Route::get('collection',function(){
-    $collection=collect([1,2,3,4,1]);
-    return $collection->duplicates();
+    //    $collection=collect([1,2,3,4,18,5,19]);
+    // return $collection->all(); // tum diziyi verir
+    // return $collection->avg(); // dizinin ortalamasini verir
+    // return $collection->count(); // dizinin eleman sayisini verir
+    // return $collection->min(); // dizinin en kucuk elemanini verir
+    // return $collection->max(); //dizinin en buyun elemanini verir
+    // return $collection->diff(3); //dizide index dekinden farkli olan degerleri verir
+    // return $collection->duplicates(); // dizide tekrarlayan elemanlari verir
+    // return $collection->except('0'); //isteneden indisi haric tutarak digerlerini verir
+    // return $collection->only(0,3); //isteneden indisi verir
+    /** 
+    $filtered=$collection->filter(function($value){
+        return $value<18; //18 yasindan kucuk olanlari cagiriyoruz
+    });
+    return $filtered->all();
+   */
+    /** 
+        $filtered=$collection->filter(function($value){
+            return $value<18; //18 yasindan kucuk olanlari cagiriyoruz
+        });
+    //    return $filtered->first();
+    // return $filtered->last();
+    */
+     /** 
+    $collection=collect([
+        'name'=> 'busra',
+        'lastname'=> 'soft',
+        'school'=> 'schools',
+    ]);
+        return $collection->flatten(); //verileri cagiriyoruz
+    */
+        /** 
+    $collection=collect([
+        'name'=> 'busra',
+        'lastname'=> 'soft',
+        'school'=> 'schools',
+    ]);
+    $collection->forget('name'); //name degerini unutturuyoruz
+        return $collection->all(); //kalan verileri cagiriyoruz
+     */
+/** 
+    $collection=collect([
+        'name'=> 'busra',
+        'lastname'=> 'soft',
+        'school'=> 'schools',
+    ]);
+    return $collection->get('name'); //get icerigindeki elamani getirir
+  */
+/** 
+  $collection=collect([
+    ]);
+    return 'Sonuc '.$collection->isEmpty('name'); //isEmpty bosa true (1) degeri getirir
+*/
+/**
+$collection=collect([
+    'name'=> 'busra',
+    'lastname'=> 'soft',
+    'school'=> 'schools',
+    ]);
+    return 'Sonuc '.$collection->isNotEmpty('name'); //isNotEmpty doluysa true (1) degeri getirir
+*/
+
+/** 
+$collection=collect([
+    ['id'=>'1','name'=> 'busra'],
+    ['id'=>'2','name'=> 'soft'],
+    ['id'=>'3','name'=> 'schools'],
+    ['id'=>'4','name'=> 'tree'],
+    ]);
+    $plucked=$collection->pluck('id','name'); //pluck cagrilan degeri getirir
+    return $plucked->all();
+*/
+/**
+    $collection=collect([
+    ['id'=>'1','name'=> 'busra'],
+    ['id'=>'2','name'=> 'soft'],
+    ['id'=>'3','name'=> 'schools'],
+    ['id'=>'4','name'=> 'tree'],
+    ]);
+    $collection->pop(); //pop son degeri siler digerlerini getirir
+    return $collection->all();  
+ */
+
+// $collection=collect([
+//     ['id'=>'1','name'=> 'busra'],
+//     ['id'=>'2','lastname'=> 'soft'],
+//     ['id'=>'3','lesson'=> 'schools'],
+//     ['id'=>'4','person
+//     '=> 'tree'],
+//     ]);
+//     $collection->pull('name'); //pop son degeri siler digerlerini getirir
+//     return $collection->all();  
+
+/** 
+$collection=collect([1,2,3,4]);
+    $collection->prepend('asdf'); //prepend 'lk basa yeni degeri ekler hepsini getirir
+    return $collection->all();  
+*/
+/**
+    $collection=collect([1,2,3,4]);
+    $collection->prepend('asdf'); //prepend 'lk basa yeni degeri ekler hepsini getirir
+    return $collection->all();  
+ */
+
+// $collection=collect([1,2,3,4,5,6,7,8,9,10]);
+// return $collection->random(); //random rastgele diziden bir deger getirir
+
+// $collection=collect([1,2,3,4,5,6,7,8,9,10]);
+//  return $collection->search(2); //search icindeki degerin kacinci indisste oldugunu veriyor
+
+// $collection=collect([1,2,3,4,5,6,7,8,9,10]);
+// $arg=$collection->shuffle(); //shuffle dizideki degerlerin yerlerini karistiriyor
+//  return $arg->all(2);
+
+// $collection=collect([10,5,3,4,2,6,7,8,9,1 ]);
+// return $collection->sort()->values()->all(); //sort dizideki degerlerin yerlerini value degerlerine gore siraliyor
+
+// $collection=collect([10,5,3,4,2,6,7,8,9,1 ]);
+// return $collection->sort()->values()->all(); //sort dizideki degerlerin yerlerini value degerlerine gore siraliyor
+
+// $collection=collect([
+//     ['id'=>'1','name'=> 'busra'],
+//     ['id'=>'2','lastname'=> 'soft'],
+//     ['id'=>'3','lesson'=> 'schools'],
+//     ['id'=>'4','person'=> 'tree'],
+// ]);
+// return $collection->sortBy('id')->values()->all(); //sortBy dizideki degerleri sirayla siraliyor
+
+// $collection=collect([
+//     ['id'=>'1','name'=> 'busra'],
+//     ['id'=>'2','lastname'=> 'soft'],
+//     ['id'=>'3','lesson'=> 'schools'],
+//     ['id'=>'4','person'=> 'tree'],
+// ]);
+// return $collection->sortByDesc('id')->values()->all();//sortByDesc dizideki degerleri testen siraliyor
+
+// $collection=collect([10,5,3,4,2,6,7,8,9,1 ]);
+// return $collection->sum(); // toplama islemi gerceklestirir
+
+// $collection=collect([10,5,3,4,2,6,7,8,9,10,5 ]);
+//  return $collection->unique(); // unique ayni olanlari cikarir 
+
+ $collection=collect([10,5,3,4,2,6,7,8,9,10,5 ]);
+ return $collection->unique(); // unique ayni olanlari cikarir 
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
