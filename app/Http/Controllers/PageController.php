161eca5892df8db2393341403b9441514f76c909
unknown <busrasof@gmail.com>
2020-02-24 12:01:46 +0300

    where call from database

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,10 +6,18 @@ Class PageController extends Controller
 {
     public function index()
     {
-        $blog=DB::table('blog')->get(); //veri tabanindan verileri getirme
-        foreach ($blog as $key ) {
-            echo $key->blog_title."<br>";
-        }
+        // $blog=DB::table('blog')->get(); //veri tabanindan verileri getirme
+        // foreach ($blog as $key ) {
+        //     echo $key->blog_title."<br>";
+        // }
+
+        $blog=DB::table('blog')
+        ->where('blog_id',2)
+        ->first(); // data get from Database
+        echo $blog->blog_title;
+        echo '<br>';
+        echo $blog->blog_content;
+        
     }
 
     public function show(){
