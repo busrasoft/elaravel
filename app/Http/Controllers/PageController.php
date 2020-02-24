1fa568905de9998492345b803c2e190cefbb58bd
unknown <busrasof@gmail.com>
2020-02-24 12:05:22 +0300

    orwhere

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -11,12 +11,19 @@ Class PageController extends Controller
         //     echo $key->blog_title."<br>";
         // }
 
+        // $blog=DB::table('blog')
+        // ->where('blog_id',2)
+        // ->first(); 
+        // echo $blog->blog_title;
+        // echo '<br>';
+        // echo $blog->blog_content;
+        
         $blog=DB::table('blog')
-        ->where('blog_id',2)
-        ->first(); // data get from Database
-        echo $blog->blog_title;
-        echo '<br>';
-        echo $blog->blog_content;
+        ->where('blog_id',2) //blog id si 2 olanlari getir
+        ->orwhere('blog_id',3) //blog id si 3 olanlari getir
+        ->orwhere('blog_id',4) //blog id si 4 olanlari getir
+        ->get(); // data get from Database
+       dd($blog);
         
     }
 
